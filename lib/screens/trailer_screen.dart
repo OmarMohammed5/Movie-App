f11ff3a66dfe95a885b3e11edbf61d601c6e202b
omarm335 <sdvopjghjervk@gmail.com>
2025-09-07 15:37:51 +0300

    update trailer screen

--- lib/screens/trailer_screen.dart
+++ lib/screens/trailer_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gap/flutter_gap.dart';
+import 'package:hugeicons/hugeicons.dart';
 import 'package:moviee_app/theme/app_colors.dart';
 import 'package:moviee_app/theme/app_text_style.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
@@ -33,17 +36,85 @@ class _TrailerScreenState extends State<TrailerScreen> {
         automaticallyImplyLeading: false,
         backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
         centerTitle: true,
-        title: AppText(
-          "Trailer",
-          color: Colors.white,
-          fontSize: 22,
-          fontWeight: FontWeight.bold,
+        title: Row(
+          spacing: 6,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AppText(
+              "Trailer",
+              color: Colors.white,
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+            HugeIcon(
+              icon: HugeIcons.strokeRoundedYoutube,
+              color: AppColors.kLogoColor,
+              size: 25,
+            ),
+          ],
         ),
       ),
-      body: YoutubePlayer(
-        controller: _controller,
-        progressIndicatorColor: AppColors.kLogoColor,
-        showVideoProgressIndicator: true,
+      body: Column(
+        children: [
+          YoutubePlayer(
+            aspectRatio: 16 / 9,
+            controller: _controller,
+            progressIndicatorColor: AppColors.kLogoColor,
+            showVideoProgressIndicator: true,
+          ),
+
+          // Gap(12),
+          // //// Movie Title & Year & Favorite
+          // Padding(
+          //   padding: const EdgeInsets.symmetric(horizontal: 16),
+          //   child: Row(
+          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //     children: [
+          //       AppText(
+          //         "Movie Title (year)",
+          //         fontSize: 18,
+          //         fontWeight: FontWeight.bold,
+          //         color: Colors.white,
+          //       ),
+          //       IconButton(
+          //         onPressed: () {},
+          //         icon: Icon(CupertinoIcons.heart_solid, color: Colors.red),
+          //       ),
+          //     ],
+          //   ),
+          // ),
+
+          // Gap(8),
+          // //// Type of Movie
+          // Padding(
+          //   padding: const EdgeInsets.symmetric(horizontal: 16),
+          //   child: Row(
+          //     children: [
+          //       Chip(
+          //         label: AppText("Type of Movie", fontSize: 14),
+          //         backgroundColor: Colors.grey[800],
+          //       ),
+          //     ],
+          //   ),
+          // ),
+
+          // Gap(16),
+          // ///// Overview
+          // Padding(
+          //   padding: const EdgeInsets.symmetric(horizontal: 16),
+          //   child: Row(
+          //     mainAxisAlignment: MainAxisAlignment.start,
+          //     children: [
+          //       AppText(
+          //         "Overview.............................",
+          //         color: Colors.white70,
+          //         fontSize: 14,
+          //         maxLines: 7,
+          //       ),
+          //     ],
+          //   ),
+          // ),
+        ],
       ),
     );
   }
