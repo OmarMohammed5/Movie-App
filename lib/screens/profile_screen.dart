6e226ac9cda3c09ff79ab0c5e596d5d5e8f464fa
omarm335 <sdvopjghjervk@gmail.com>
2025-09-09 05:57:35 +0300

    create profile content widget

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -4,15 +4,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gap/flutter_gap.dart';
 import 'package:hugeicons/hugeicons.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:moviee_app/components/auth_required_dialog.dart';
-import 'package:moviee_app/components/image_profile.dart';
-import 'package:moviee_app/components/name_and_email_profile.dart';
-import 'package:moviee_app/components/profile_menu.dart';
+import 'package:moviee_app/components/profile_content.dart';
 import 'package:moviee_app/core/cubit/cubit/auth_cubit.dart';
-import 'package:moviee_app/core/cubit/cubit/bottom_nav_cubit.dart';
 import 'package:moviee_app/core/cubit/cubit/profile_cubit.dart';
-import 'package:moviee_app/core/cubit/cubit/theme_cubit.dart';
-import 'package:moviee_app/screens/about_app_screen.dart';
 import 'package:moviee_app/theme/app_colors.dart';
 import 'package:moviee_app/theme/app_text_style.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -25,44 +19,7 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  // logic function for upload image
-
   XFile? selectedImage;
-
-  /// upload image
-  Future<void> _uploadImage() async {
-    final user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
-      showDialog(context: context, builder: (context) => AuthRequiredDialog());
-      return;
-    }
-
-    /// if user have an account
-    final pickedImage = await ImagePicker().pickImage(
-      source: ImageSource.gallery,
-    );
-    final isUpload = selectedImage == null;
-
-    if (pickedImage != null) {
-      setState(() {
-        selectedImage = pickedImage;
-      });
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: AppText(
-            isUpload
-                ? " Image Uploaded Successfully"
-                : "Image Changed Successfully",
-            color: Colors.white,
-          ),
-          backgroundColor: Colors.black,
-        ),
-      );
-
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.setString('profile_image_${user.uid}', pickedImage.path);
-    }
-  }
 
   /// Saved Image with SharedPreferences
   Future<void> _savedImage() async {
@@ -91,9 +48,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final themeCubit = context.watch<ThemeCubit>();
-    final isDarkMode = themeCubit.state == ThemeMode.dark; //  default
-
     return BlocListener<AuthCubit, AuthState>(
       listener: (context, state) {
         if (state is AuthLoaded) {
@@ -125,103 +79,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 ),
               );
             } else if (state is AuthLoaded) {
-              return SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Gap(20),
-                    // Image profile
-                    ImageProfile(selectedImage: selectedImage),
-                    Gap(10),
-                    // upload image
-                    GestureDetector(
-                      onTap: _uploadImage,
-
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 110),
-                        child: Container(
-                          padding: EdgeInsets.all(6),
-
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            spacing: 8,
-                            children: [
-                              AppText(
-                                selectedImage == null
-                                    ? "Upload Image"
-                                    : "Change Image",
-                                color: AppColors.kLogoColor,
-                                fontSize: 14,
-                              ),
-                              Icon(
-                                //// Upload or change photo
-                                selectedImage == null
-                                    ? Icons.add_photo_alternate_outlined
-                                    : Icons.edit,
-                                color: AppColors.kLogoColor,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    Gap(70),
-
-                    //// Name && Email
-                    NameAndEmailProfile(),
-                    Gap(10),
-
-                    // Profile Menu Section
-                    ProfileMenu(
-                      icon: isDarkMode
-                          ? HugeIcons.strokeRoundedSun02
-                          : HugeIcons.strokeRoundedMoon02,
-                      title: isDarkMode ? "Light Mode" : "Dark Mode",
-                      onTap: () {
-                        context.read<ThemeCubit>().toggleTheme();
-                      },
-                    ),
-                    ProfileMenu(
-                      icon: HugeIcons.strokeRoundedInformationCircle,
-                      title: "About App",
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return AboutAppScreen();
-                            },
-                          ),
-                        );
-                      },
-                    ),
-                    ProfileMenu(
-                      icon: HugeIcons.strokeRoundedLogout02,
-                      title: "Logout",
-                      color: Colors.redAccent,
-                      onTap: () {
-                        context.read<AuthCubit>().logout(context);
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: AppText(
-                              "Logout Successfully",
-                              color: Colors.white,
-                            ),
-                            backgroundColor: Colors.black,
-                          ),
-                        );
-                        context.read<BottomNavCubit>().changeTab(0);
-                        setState(() {
-                          selectedImage = null;
-                        });
-                      },
-                    ),
-                  ],
-                ),
-              );
+              return ProfileContent();
             } else {
               return Center(
                 child: Column(
