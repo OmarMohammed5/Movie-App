28d12167041cdf311de2bc2534ee1ad5f1baf357
omarm335 <sdvopjghjervk@gmail.com>
2025-10-08 02:36:45 +0300

    update trailer screen and display full screen for videos

--- lib/screens/trailer_screen.dart
+++ lib/screens/trailer_screen.dart
@@ -16,7 +16,7 @@ class TrailerScreen extends StatefulWidget {
 
 class _TrailerScreenState extends State<TrailerScreen> {
   late YoutubePlayerController _controller;
-
+  bool _isFullScreen = false;
   @override
   void initState() {
     super.initState();
@@ -24,39 +24,47 @@ class _TrailerScreenState extends State<TrailerScreen> {
     _controller = YoutubePlayerController(
       initialVideoId: widget.videoKey,
       flags: const YoutubePlayerFlags(autoPlay: true),
-    );
+    )..addListener(fullScreen);
+  }
+
+  void fullScreen() {
+    if (_controller.value.isFullScreen != _isFullScreen) {
+      setState(() {
+        _isFullScreen = _controller.value.isFullScreen;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // bool isLandscape =
-    //     MediaQuery.of(context).orientation == Orientation.landscape;
-
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          spacing: 6,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            AppText("Trailer", fontSize: 22, fontWeight: FontWeight.bold),
-            HugeIcon(
-              icon: HugeIcons.strokeRoundedYoutube,
-              color: AppColors.kLogoColor,
-              size: 25,
+      appBar: _isFullScreen
+          ? null
+          : AppBar(
+              toolbarHeight: 40,
+              title: Row(
+                spacing: 6,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  AppText("Trailer", fontSize: 22, fontWeight: FontWeight.bold),
+                  HugeIcon(
+                    icon: HugeIcons.strokeRoundedYoutube,
+                    color: AppColors.kLogoColor,
+                    size: 25,
+                  ),
+                ],
+              ),
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back_ios),
+                onPressed: () {
+                  SystemChrome.setPreferredOrientations([
+                    DeviceOrientation.portraitUp,
+                    DeviceOrientation.portraitDown,
+                  ]);
+                  Navigator.pop(context);
+                },
+              ),
             ),
-          ],
-        ),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios),
-          onPressed: () {
-            SystemChrome.setPreferredOrientations([
-              DeviceOrientation.portraitUp,
-              DeviceOrientation.portraitDown,
-            ]);
-            Navigator.pop(context);
-          },
-        ),
-      ),
       body: YoutubePlayer(
         controller: _controller,
         showVideoProgressIndicator: true,
